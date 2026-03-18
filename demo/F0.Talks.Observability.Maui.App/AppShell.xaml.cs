57f882ac6d6d33e7dbadd08445347b946d992020
Stefan Pölz <38893694+Flash0ver@users.noreply.github.com>
2026-03-18 11:32:54 +0100

    feat: add .NET MAUI App

---
+++ demo/F0.Talks.Observability.Maui.App/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace F0.Talks.Observability.Maui.App;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ demo/F0.Talks.Observability.Maui.App/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace F0.Talks.Observability.Maui.App;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
